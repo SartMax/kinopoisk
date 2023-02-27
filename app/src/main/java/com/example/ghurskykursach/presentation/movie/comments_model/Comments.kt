da1299177a1f97b12f76697ad2b775667a770b72
SartMax <ngurski12@gmail.com>
2023-02-27 21:17:22 +0300

    moderation comments

--- app/src/main/java/com/example/ghurskykursach/presentation/movie/comments_model/Comments.kt
+++ app/src/main/java/com/example/ghurskykursach/presentation/movie/comments_model/Comments.kt
@@ -3,5 +3,6 @@ package com.example.ghurskykursach.presentation.movie.comments_model
 data class Comments(
     val comment_id: String,
     val movie_id : String,
-
+    val user_name: String,
+    val comment: String
 )
