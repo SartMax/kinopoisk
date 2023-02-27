963257f1669bcd8142e3c3c69528e7943ca2ec2f
SartMax <ngurski12@gmail.com>
2023-02-27 16:22:34 +0300

    test

--- app/src/main/java/com/example/ghurskykursach/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/ghurskykursach/presentation/favorite/FavoriteFragment.kt
@@ -25,6 +25,8 @@ class FavoriteFragment : Fragment() {
     ): View {
         binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         return binding.root
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
