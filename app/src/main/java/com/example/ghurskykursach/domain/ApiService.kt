016a5dbb25702a88785785d45f0d1846aa7a810d
younglord12 <sergeymoiseenko90@mail.ru>
2023-03-01 02:38:23 +0300

    ApiService.kt

--- app/src/main/java/com/example/ghurskykursach/domain/ApiService.kt
+++ app/src/main/java/com/example/ghurskykursach/domain/ApiService.kt
@@ -8,6 +8,11 @@ import retrofit2.http.Query
 
 interface ApiService {
 
+    @GET("movie?isStrict=false&field=name&token=HB6B6RM-2VC45QY-KY7D7BE-GRVSZX4")
+    suspend fun getMovieByName(
+        @Query("search") search: String
+    ): Response<Films>
+
     @GET("movie?token=HB6B6RM-2VC45QY-KY7D7BE-GRVSZX4&field=id")
     suspend fun getMovieById(
         @Query("search") id: Int
